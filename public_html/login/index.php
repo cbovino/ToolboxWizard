f192ec1692e18e735f3df8dcf88325384abd9549
Connor Bovino <connor.bovino@gmail.com>
2018-12-10 18:46:03 -0500

    change

--- public_html/login/index.php
+++ public_html/login/index.php
@@ -1,38 +1,37 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" type="text/css" href="css/LoginAndSignup.css">
+  <link rel="stylesheet" type="text/css" href="../css/LoginAndSignup.css">
   <title>Toolbox Wizard - login and register </title>
-<script>
-function validateForm(){
-  var x = document.forms["login"]["userName"].value;
-  var y = document.forms["login"]["pass"].value;
-  if(x == "" || y == ""){
-    alert("Please enter your user name or password");
-    return false;
+  <script>
+  function validateForm(){
+    var x = document.forms["login"]["userName"].value;
+    var y = document.forms["login"]["pass"].value;
+    if(x == "" || y == ""){
+      alert("Please enter your user name or password");
+      return false;
+    }
   }
-}
-function validateForm1(){
-  var z = document.forms["resgister"]["firstName"].value;
-  var a = document.forms["resgister"]["lastName"].value;
-  var b = document.forms["resgister"]["email"].value;
-  var c = document.forms["resgister"]["creatingPass"].value;
-  var d = document.forms["resgister"]["confirmPass"].value;
-  if(z == "" || a == "" || b == "" || c == "" || d == ""){
-    alert("Information is required");
-    return false;
-  } else if (c != d) {
-    alert("It isn't the same with the password. Please try again");
+  function validateForm1(){
+    var a = document.forms["register"]["userName"].value;
+    var b = document.forms["register"]["email"].value;
+    var c = document.forms["register"]["creatingPass"].value;
+    var d = document.forms["register"]["confirmPass"].value;
+    if(a == "" || b == "" || c == "" || d == ""){
+      alert("Information is required");
+      return false;
+    } else if (c != d) {
+      alert("It isn't the same with the password. Please try again");
+    }
   }
-}
-function hidePassOrNot(){
-  var x = document.getElementById("inputPass");
-  if(x.type === "password"){
-    x.type = "text";
-  } else {
-    x.type = "password";
+  function hidePassOrNot(){
+    var x = document.getElementById("inputPass");
+    if(x.type === "password"){
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
   }
-}
 </script>
 </head>
 <body>
@@ -46,20 +45,18 @@ function hidePassOrNot(){
     Password: <input type="password" name="pass" id="inputPass"> <br><br>
     <input type="checkbox" onclick="hidePassOrNot()">Show Password <br><br>
     <input type="submit" value="Login"> <br><br><br><br>
-
   </form>
 
-  <form align="center" name="resgister" action="register.php" onsubmit="return validateForm1()">
+  <form align="center" name="register" action="register.php" onsubmit="return validateForm1()" method = "POST">
     <fieldset>
       <p><b>Sign Up</b></p>
-      First name: <input type="text" name="firstName"> <br><br>
-      Last name: <input type="text" name="lastName"> <br><br>
+      Username: <input type="text" name="lastName"> <br><br>
       Email: <input type="text" name="email"> <br><br>
       Password: <input type="password" name="creatingPass"> <br><br>
       Confirm Password: <input type="password" name="confirmPass"> <br><br>
       <input type="submit" value="Submit"><br><br>
 
-  </fieldset>
+    </fieldset>
 
   </form>
 </body>
